cd18d969d44d590e330ead4fdfcdbb1225a1058e
Ma1akAs <1763611895@qq.com>
2024-03-19 22:15:20 +0800

    JWT校验配置完毕

--- src/main/java/com/zoi/config/SecurityConfiguration.java
+++ src/main/java/com/zoi/config/SecurityConfiguration.java
@@ -1,22 +1,35 @@
 package com.zoi.config;
 
 import com.zoi.entity.RestBean;
-import jakarta.servlet.ServletException;
+import com.zoi.entity.vo.response.AuthorizeVO;
+import com.zoi.filter.JwtAuthorizeFilter;
+import com.zoi.utils.JwtUtils;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Resource
+    JwtUtils jwtUtils;
+
+    @Resource
+    JwtAuthorizeFilter jwtAuthorizeFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -34,25 +47,64 @@ public class SecurityConfiguration {
                         .logoutUrl("/api/auth/logout")
                         .logoutSuccessHandler(this::onLogoutSuccess)
                 )
+                .exceptionHandling(conf -> conf
+                        .authenticationEntryPoint(this::onUnauthorized)
+                        .accessDeniedHandler(this::onAccessDeny)
+                )
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(conf -> conf
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .addFilterBefore(jwtAuthorizeFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    private void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse response, Authentication authentication) {
-    }
-
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    private void onAccessDeny(HttpServletRequest httpServletRequest,
+                              HttpServletResponse response,
+                              AccessDeniedException e) throws IOException{
         response.setContentType("application/json;charset=utf-8");
-        response.getWriter().write(RestBean.success().asJsonString());
+        response.getWriter().write(RestBean.forbidden(e.getMessage()).asJsonString());
     }
 
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+    public void onUnauthorized(HttpServletRequest request,
+                               HttpServletResponse response,
+                               AuthenticationException e) throws IOException {
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(RestBean.unAuthorize(e.getMessage()).asJsonString());
+    }
+
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        response.setContentType("application/json;charset=utf-8");
+        User user = (User) authentication.getPrincipal();
+        String token = jwtUtils.createJwt(user, 1, "sb");
+        AuthorizeVO vo = new AuthorizeVO();
+        vo.setExpire(jwtUtils.expireTime());
+        vo.setRole("");
+        vo.setToken(token);
+        vo.setUsername("sb");
+        response.getWriter().write(RestBean.success(vo).asJsonString());
+    }
+
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
         response.setContentType("application/json;charset=utf-8");
         response.getWriter().write(RestBean.failure(401, exception.getMessage()).asJsonString());
     }
 
+    private void onLogoutSuccess(HttpServletRequest httpServletRequest,
+                                 HttpServletResponse response,
+                                 Authentication authentication) throws IOException{
+        response.setContentType("application/json;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        String authorization = httpServletRequest.getHeader("Authorization");
+        if (jwtUtils.invalidateJwt(authorization)) {
+            writer.write(RestBean.success().asJsonString());
+        } else {
+            writer.write(RestBean.failure(400, "退出失败").asJsonString());
+        }
+    }
 
 }
---
+++ src/main/java/com/zoi/utils/Const.java
@@ -0,0 +1,5 @@
+package com.zoi.utils;
+
+public class Const {
+    public static final String JWT_BLACK_LIST = "jwt:blacklist:";
+}
